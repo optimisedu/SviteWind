63ec226619d9991a8151ab4ae54b3d24150b2c2c
optimiseu <optimisedu@gmail.com>
2022-10-23 11:55:10 +0100

    errorfulprogress.?

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,10 @@
 <script>
-// import { Lowdash } from "lowdash";
 import "./app.postcss";
-let input;
-let output
-let error = "still not working";
+import Toggle from "./lib/Toggle.svelte";
+import _ from "lodash-es"
 
-const handleInput = (input, output, error) => {
-  input = document.getElementById("iput");
-  output = document.getElementById("output");
-  try {
-    output = JSON.stringify(JSON.parse(input), null, 2);
-  } catch (e) {
-    error = e;
-  }
-};
-
-const toggle = () => {true ? false : true};
+let iput= " " ;
+let output;
 </script>
 
 <main class = "grid-flow-col">
@@ -28,36 +17,42 @@ const toggle = () => {true ? false : true};
   <div class="mb-4 w-full bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
     <div class="pt-2 px-4 bg-whiter rounded-t-lg dark:bg-gray-800 ">
       <label for="comment" class="sr-only">
-        {input}
+        {iput}
       </label>
       <textarea 
-        bind:value={input}
+        bind:value={iput}
         id="iput"
         rows="6"
         class="px-0 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
-        placeholder="{input}"
+        placeholder="{iput}"
         ></textarea>
     </div>
   </div>
 </form>
-<form class="w-screen px -inset-5 px-5" >
+
+
+ <div class ="grid place-content-center">
+<Toggle white bind:value={white}></Toggle>
+</div>
+
 
 
   <div class ="flex justify-center">
-  <button type="button" class="Fflex justify-self-center focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-6 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">convert</button>
+  <button type="button" class="flex justify-self-center focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-6 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" on:click={whiteSpace}>convert</button>
   </div>
 
+
+  <form class="w-screen px -inset-5 px-5" >
   <div class="grid mb-4 w-full bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
     <div class="py-2 px-4 bg-whiter rounded-t-lg dark:bg-gray-800 ">
       <label for="comment" class="sr-only">
-        {output}
+        {iput}
       </label>
       <textarea
-      bind:value={input}
         id="iput"
         rows="6"
         class="px-0 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
-        placeholder='{output}'
+        placeholder='{iput}'
         disabled = true
       ></textarea>
     </div>
---
+++ src/lib/Upload.svelte
@@ -0,0 +1,16 @@
+<script>
+export let files;
+$: if (files) {
+  console.log(files);
+}
+</script>
+
+<label for="avatar">Upload your css</label>
+
+  <input
+    accept="text/plain"
+    bind:files
+    id="css"
+    name="css"
+    type="file"
+  />
