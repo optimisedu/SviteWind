90e88ede4bb18b4d1083c538bf136ff400e496fb
optimiseu <optimisedu@gmail.com>
2022-10-24 05:34:55 +0100

    Will revisit when replaceAll is more widely supported (es7/21)

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,13 @@
 <script>
 // import { Lowdash } from "lowdash";
 import "./app.postcss";
-let input;
-let output
+let iput;
+let output;
 let error = "still not working";
-
-const handleInput = (input, output, error) => {
-  input = document.getElementById("iput");
-  output = document.getElementById("output");
-  try {
-    output = JSON.stringify(JSON.parse(input), null, 2);
-  } catch (e) {
-    error = e;
-  }
-};
-
-const toggle = () => {true ? false : true};
+function convert(iput, output) {
+  return output = iput.replaceAll(" ", "")
+}
+// $: output = iput.replaceAll(" ", "");
 </script>
 
 <main class = "grid-flow-col">
@@ -28,14 +20,14 @@ const toggle = () => {true ? false : true};
   <div class="mb-4 w-full bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
     <div class="pt-2 px-4 bg-whiter rounded-t-lg dark:bg-gray-800 ">
       <label for="comment" class="sr-only">
-        {input}
+        {iput}
       </label>
       <textarea 
-        bind:value={input}
+        bind:value={iput}
         id="iput"
         rows="6"
         class="px-0 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
-        placeholder="{input}"
+        placeholder="{iput}"
         ></textarea>
     </div>
   </div>
@@ -44,7 +36,7 @@ const toggle = () => {true ? false : true};
 
 
   <div class ="flex justify-center">
-  <button type="button" class="Fflex justify-self-center focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-6 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">convert</button>
+  <button type="button" class="Fflex justify-self-center focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-6 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" on:click={convert}>Convert</button>
   </div>
 
   <div class="grid mb-4 w-full bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
@@ -53,8 +45,7 @@ const toggle = () => {true ? false : true};
         {output}
       </label>
       <textarea
-      bind:value={input}
-        id="iput"
+        id="outpt"
         rows="6"
         class="px-0 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
         placeholder='{output}'
